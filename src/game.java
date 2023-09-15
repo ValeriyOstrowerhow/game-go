99b485d1ef86606261d60e73067df0b666d6742c
Vadim Shalamov <reetvvy@gmail.com>
2023-09-15 18:18:48 +0500

    Feat: adding a substring method that looks at the correctness of the place in the field where you want to place the stone, as well as implementing the output of the pass method as a separate one and improving the program as a whole

--- src/game.java
+++ src/game.java
@@ -1,10 +1,7 @@
 import java.util.Scanner;
 
 public class game {
-
-  private static String element;
- // private static StonesAndPartsOfTheField[][] field = new StonesAndPartsOfTheField()[10][10];
- private static String[][] field = new String[10][10];
+  private static String[][] field = new String[10][10];
   private static byte PlayerNumber = 0;
   public static int countpass = 0;
 
@@ -42,51 +39,20 @@ public class game {
   public static boolean addingStonesToTheField(Scanner scanner, boolean isRun2) {
     nextPlayer();
     System.out.println("Сейчас ход " + (1 == PlayerNumber ? "чёрных!" : "белых!"));
-
-    while (true) {
-      System.out.print("Введите \"+\" чтобы сделать пасс и \"-\" чтобы его не делать: ");
-      String pass = scanner.nextLine();
-      String plus = "+";
-      String minus = "-";
-
-      if (pass.equals(plus)) {
-        countpass++;
-        game.PrintingTheField();
-        isRun2 = false;
-        return isRun2;
-      } else if (pass.equals(minus)) {
-        countpass = 0;
-        break;
-      } else {
-        System.out.println("Вы ввели некоректное значение сделайте ввод ещё раз!");
-      }
+    isRun2 = game.Pass(scanner, isRun2);
+    if (isRun2 == false) {
+      return isRun2;
     }
-
     System.out.print("Введите поле на которое хотите поставить камень: ");
 
     while (scanner.hasNext()) {
       String place = scanner.nextLine().toLowerCase();
-      char x = place.charAt(0);
-      if (x > 57 || x < 48) {
-        String letter = String.valueOf(place.charAt(0));
-        String number = String.valueOf(place.charAt(1));
-        game.substring(place, letter, number);
-        isRun2 = game.auxiliaryMethodForAddingStones(number, letter, place, isRun2);
-        if (isRun2 == false) {
-          return isRun2;
-        }
-      } else {
-        String letter = String.valueOf(place.charAt(1));
-        String number = String.valueOf(place.charAt(0));
-        if (letter.equals("a") || letter.equals("b") || letter.equals("c") || letter.equals("d") || letter.equals("e")
-            || letter.equals("f") || letter.equals("g") || letter.equals("h") || letter.equals("i")) {
-          isRun2 = game.auxiliaryMethodForAddingStones(number, letter, place, isRun2);
-          if (isRun2 == false) {
-            return isRun2;
-          }
-        } else {
-          System.out.print("Введите поле на которое хотите поставить камень: ");
-        }
+      String[] plaseinfield = game.substring(place);
+      String letter = plaseinfield[0];
+      String number = plaseinfield[1];
+      isRun2 = game.auxiliaryMethodForAddingStones(number, letter, place, isRun2);
+      if (isRun2 == false) {
+        return isRun2;
       }
     }
     return isRun2;
@@ -111,7 +77,7 @@ public class game {
     }
   }
 
-  private static boolean Pass(Scanner scanner, boolean isRun) {
+  private static boolean Pass(Scanner scanner, boolean isRun2) {
     while (true) {
       System.out.print("Введите \"+\" чтобы сделать пасс и \"-\" чтобы его не делать: ");
       String pass = scanner.nextLine();
@@ -121,7 +87,8 @@ public class game {
       if (pass.equals(plus)) {
         countpass++;
         game.PrintingTheField();
-        isRun = false;
+        isRun2 = false;
+        break;
       } else if (pass.equals(minus)) {
         countpass = 0;
         break;
@@ -129,7 +96,7 @@ public class game {
         System.out.println("Вы ввели некоректное значение сделайте ввод ещё раз!");
       }
     }
-    return isRun;
+    return isRun2;
   }
 
   private static boolean auxiliaryMethodForAddingStones(String number, String letter, String place, boolean isRun2) {
@@ -156,15 +123,31 @@ public class game {
     PlayerNumber = (byte) (1 == PlayerNumber ? 2 : 1);
   }
 
-  private static void substring(String place, String letter, String number) {
+  private static String[] substring(String place) {
+    String[] plaseinfield = new String[2];
     if (place.length() == 2) {
-      if (letter.equals("a") || letter.equals("b") || letter.equals("c") || letter.equals("d") || letter.equals("e")
-          || letter.equals("f") || letter.equals("g") || letter.equals("h") || letter.equals("i")) {
+      char x = place.charAt(0); // letter
+      char y = place.charAt(1); // number
+      if ((x >= 97 && x <= 105) && (y >= 49 && y <= 57)) { // 49 = 1/ 57 = 9
+        String letter = String.valueOf(place.charAt(0));
+        String number = String.valueOf(place.charAt(1));
+        plaseinfield[0] = letter;
+        plaseinfield[1] = number;
+      } else if ((y >= 97 && y <= 105) && (x >= 49 && x <= 57)) {
+        String letter = String.valueOf(place.charAt(1));
+        String number = String.valueOf(place.charAt(0));
+        plaseinfield[0] = letter;
+        plaseinfield[1] = number;
       } else {
-      System.out.print("Даннаго места на поле нет! Ведите другое место:");
-    }
+        System.out.print("Даннаго места на поле нет! Ведите другое место: ");
+        plaseinfield[0] = " ";
+        plaseinfield[1] = " ";
+      }
     } else {
-      System.out.print("Даннаго места на поле нет! Ведите другое место:");
+      System.out.print("Даннаго места на поле нет! Ведите другое место: ");
+      plaseinfield[0] = " ";
+      plaseinfield[1] = " ";
     }
+    return plaseinfield;
   }
 }
